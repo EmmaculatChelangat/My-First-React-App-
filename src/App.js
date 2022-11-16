989471ff5d5745818746710002681f3eb62c9322
ChelangatEmmaculat <chelangatemmaculat2744@gmail.com>
2022-11-16 18:20:11 +0300

    my-first-react-app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
 
-function App() {
+// const Person = (props) => {
+//   return (
+//     <>
+//       <h1>First Name: Emmaculat</h1>
+//       <h2>Last Name: Chelangat</h2>
+//       <h2>Surname: Chelaa</h2>
+//       <h2>Age: 22</h2>
+
+//       <h1>First Name: {props.fname}</h1>
+//       <h2>Last Name: {props.lname}</h2>
+//       <h2>Course: {props.course}</h2>
+//       <h2>Registration: {props.registration}</h2>
+//     </>
+//   );
+// };
+
+// const App = () => {
+//   return (
+//     <div className="App">
+//       {/* <Person /> */}
+//       <Person
+//         fname={"Jill"}
+//         lname={"Chepkirui"}
+//         course={"Computer Science"}
+//         registration={"CSC"}
+//       />
+//       <Person
+//         fname={"Brandon"}
+//         lname={"Kiptoo"}
+//         course={"Education"}
+//         registration={"EDS"}
+//       />
+//     </div>
+//   );
+// };
+const App = () => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    setCounter(100)
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
+        -
+      </button>
+      <h1>{counter}</h1>
+      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
+        +
+      </button>
     </div>
   );
-}
+};
 
 export default App;
